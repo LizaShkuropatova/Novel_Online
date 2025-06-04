5e695579a72ffcdda930f5a3e89ee72bc2a2a913
Shkuropatova Liza <101864211+LizaShkuropatova@users.noreply.github.com>
2025-06-04 13:13:16 +0300

    Add files via upload

--- novel_app_frontend/pages/fixture/index.vue
+++ novel_app_frontend/pages/fixture/index.vue
@@ -1,1 +1,11 @@
-
+<template>
+ <Head>
+  <PageMeta
+   title="Components"
+   description="Components Examples for testing."
+  />
+ </Head>
+ <NuxtLayout>
+  <ComponentsCatalog />
+ </NuxtLayout>
+</template>
