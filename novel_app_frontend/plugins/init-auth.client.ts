008b5f2f79881a2ad044c8e52f590a8d670739b2
Shkuropatova Liza <101864211+LizaShkuropatova@users.noreply.github.com>
2025-06-04 13:05:50 +0300

    Add files via upload

--- novel_app_frontend/plugins/init-auth.client.ts
+++ novel_app_frontend/plugins/init-auth.client.ts
@@ -1,1 +1,10 @@
+import { useAuthStore } from '@/store/auth';
 
+export default defineNuxtPlugin(async () => {
+ const auth = useAuthStore();
+ const token = useCookie('access_token').value;
+
+ if (token) {
+  await auth.fetchProfile();
+ }
+});
