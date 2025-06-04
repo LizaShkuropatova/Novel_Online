beff666f190a212b7da4ea56ed31770948ffbebf
Shkuropatova Liza <101864211+LizaShkuropatova@users.noreply.github.com>
2025-06-04 13:53:21 +0300

    Add files via upload

--- novel_app_frontend/components/docs/Intro.vue
+++ novel_app_frontend/components/docs/Intro.vue
@@ -1,1 +1,19 @@
-
+<template>
+ <b-div padding="y-3">
+  <ViewState
+   v-slot="meta"
+   src="route://meta"
+  >
+   <b-h
+    level="1"
+    margin="y-3"
+    display-headings="3"
+   >
+    {{ meta.data.title }}
+   </b-h>
+   <p class="lead">
+    {{ meta.data.description }}
+   </p>
+  </ViewState>
+ </b-div>
+</template>
