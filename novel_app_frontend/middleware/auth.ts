aebcd09b24a3da742c08f412f661f346ee8a5634
Shkuropatova Liza <101864211+LizaShkuropatova@users.noreply.github.com>
2025-06-04 13:13:57 +0300

    Add files via upload

--- novel_app_frontend/middleware/auth.ts
+++ novel_app_frontend/middleware/auth.ts
@@ -1,1 +1,15 @@
+import { useAuthStore } from '@/store/auth';
 
+export default defineNuxtRouteMiddleware(async (to, from) => {
+ const auth = useAuthStore();
+
+ // If token is present, but profile is not loaded - fetch profile data
+ if (auth.isAuthenticated && !auth.user) {
+  await auth.fetchProfile();
+ }
+
+ // If is not authenticated - redirect to main page
+ if (!auth.isAuthenticated) {
+  return navigateTo('/');
+ }
+});
